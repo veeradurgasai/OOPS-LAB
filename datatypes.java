8f8bf4df47d958908736731a479588b1a71bf22f
veeradurgasai <veeradurgasai784@gmail.com>
2026-01-04 16:06:06 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,12 @@
+public class datatypes{
+public static void main(String[] args){
+int quant = 2;
+String name = "ICECREAM";
+double cost = 20.05;
+boolean ice = true;
+double t = quant * cost;
+System.out.println("ITEM NEED STATUS : " + ice);
+System.out.println("ITEM NEED : " + name);
+System.out.println("TOTAL AMOUNT PAYABLE : " + t);
+}
+}
